b4e6e5d31d10caefa75ebb2c69790906e6f996d2
Urvashi Dhingra <dhingraurvashi99@gmail.com>
2020-07-01 23:26:49 +0530

    game four

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{ 
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = Input.mousePosition;
+        position.z = -Camera.main.transform.position.z;
+        position = Camera.main.ScreenToWorldPoint(position);
+
+        transform.position = position;
+    }
+}
